Project-Kaleidoscope/android_packages_apps_Launcher3

Introduce memory info in overview
This is convenient for monitoring device status and many OEMs have
their design for this. It's time to have our own one.

Change-Id: I890c86c798055b1964506d9a4df1cef386562160
Signed-off-by: LibXZR <i@xzr.moe>

quickstep/src/com/android/launcher3/BaseQuickstepLauncher.java
@@ -70,6 +70,7 @@ import com.android.quickstep.TouchInteractionService.TISBinder;
 import com.android.quickstep.util.RemoteAnimationProvider;
 import com.android.quickstep.util.RemoteFadeOutAnimationListener;
 import com.android.quickstep.util.SplitSelectStateController;
+import com.android.quickstep.views.MemInfoView;
 import com.android.quickstep.views.MidClearAllButton;
 import com.android.quickstep.views.OverviewActionsView;
 import com.android.quickstep.views.RecentsView;
@@ -99,6 +100,7 @@ public abstract class BaseQuickstepLauncher extends Launcher
 
     private OverviewActionsView mActionsView;
     private MidClearAllButton mMidClearAllButton;
+    private MemInfoView mMemInfoView;
 
     private @Nullable TaskbarManager mTaskbarManager;
     private @Nullable OverviewCommandHelper mOverviewCommandHelper;
@@ -163,6 +165,9 @@ public abstract class BaseQuickstepLauncher extends Launcher
         }
         if (mMidClearAllButton != null) {
             mMidClearAllButton.updateVerticalMargin(newMode);
+        }
+        if (mMemInfoView != null) {
+            mMemInfoView.updateVerticalMargin(newMode);
         }
     }
 
@@ -279,15 +284,18 @@ public abstract class BaseQuickstepLauncher extends Launcher
         mActionsView = findViewById(R.id.overview_actions_view);
         mSplitPlaceholderView = findViewById(R.id.split_placeholder);
         mMidClearAllButton = findViewById(R.id.mid_clear_all);
+        mMemInfoView = findViewById(R.id.meminfo);
         RecentsView overviewPanel = (RecentsView) getOverviewPanel();
         mSplitPlaceholderView.init(
                 new SplitSelectStateController(mHandler, SystemUiProxy.INSTANCE.get(this))
         );
-        overviewPanel.init(mActionsView, mSplitPlaceholderView, mMidClearAllButton);
+        overviewPanel.init(mActionsView, mSplitPlaceholderView, mMidClearAllButton, mMemInfoView);
         mActionsView.setDp(getDeviceProfile());
         mMidClearAllButton.setDp(getDeviceProfile());
+        mMemInfoView.setDp(getDeviceProfile());
         mActionsView.updateVerticalMargin(SysUINavigationMode.getMode(this));
         mMidClearAllButton.updateVerticalMargin(SysUINavigationMode.getMode(this));
+        mMemInfoView.updateVerticalMargin(SysUINavigationMode.getMode(this));
 
         mAppTransitionManager = new QuickstepTransitionManager(this);
         mAppTransitionManager.registerRemoteAnimations();
@@ -306,6 +314,10 @@ public abstract class BaseQuickstepLauncher extends Launcher
 
     public MidClearAllButton getMidClearAllButton () {
         return mMidClearAllButton;
+    }
+
+    public MemInfoView getMemInfoView () {
+        return mMemInfoView;
     }
 
     public SplitPlaceholderView getSplitPlaceholderView() {

quickstep/src/com/android/quickstep/RecentsActivity.java
@@ -74,6 +74,7 @@ import com.android.quickstep.fallback.RecentsState;
 import com.android.quickstep.SysUINavigationMode;
 import com.android.quickstep.util.RecentsAtomicAnimationFactory;
 import com.android.quickstep.util.SplitSelectStateController;
+import com.android.quickstep.views.MemInfoView;
 import com.android.quickstep.views.MidClearAllButton;
 import com.android.quickstep.views.OverviewActionsView;
 import com.android.quickstep.views.RecentsView;
@@ -105,6 +106,7 @@ public final class RecentsActivity extends StatefulActivity<RecentsState> {
     private FallbackRecentsView mFallbackRecentsView;
     private OverviewActionsView mActionsView;
     private MidClearAllButton mMidClearAllButton;
+    private MemInfoView mMemInfoView;
 
     private Configuration mOldConfig;
 
@@ -124,6 +126,7 @@ public final class RecentsActivity extends StatefulActivity<RecentsState> {
         mFallbackRecentsView = findViewById(R.id.overview_panel);
         mActionsView = findViewById(R.id.overview_actions_view);
         mMidClearAllButton = findViewById(R.id.mid_clear_all);
+        mMemInfoView= findViewById(R.id.meminfo);
         SYSUI_PROGRESS.set(getRootView().getSysUiScrim(), 0f);
 
         SplitPlaceholderView splitPlaceholderView = findViewById(R.id.split_placeholder);
@@ -132,10 +135,13 @@ public final class RecentsActivity extends StatefulActivity<RecentsState> {
         );
 
         mDragLayer.recreateControllers();
-        mFallbackRecentsView.init(mActionsView, splitPlaceholderView, mMidClearAllButton);
+        mFallbackRecentsView.init(mActionsView, splitPlaceholderView, mMidClearAllButton, mMemInfoView);
 
         mMidClearAllButton.setDp(mDeviceProfile);
         mMidClearAllButton.updateVerticalMargin(SysUINavigationMode.getMode(this));
+
+        mMemInfoView.setDp(mDeviceProfile);
+        mMemInfoView.updateVerticalMargin(SysUINavigationMode.getMode(this));
     }
 
     @Override
@@ -199,6 +205,10 @@ public final class RecentsActivity extends StatefulActivity<RecentsState> {
 
     public MidClearAllButton getMidClearAllButton() {
         return mMidClearAllButton;
+    }
+
+    public MemInfoView getMemInfoView() {
+        return mMemInfoView;
     }
 
     @Override

quickstep/src/com/android/quickstep/fallback/FallbackRecentsView.java
@@ -37,6 +37,7 @@ import com.android.launcher3.testing.TestProtocol;
 import com.android.quickstep.FallbackActivityInterface;
 import com.android.quickstep.GestureState;
 import com.android.quickstep.RecentsActivity;
+import com.android.quickstep.views.MemInfoView;
 import com.android.quickstep.views.MidClearAllButton;
 import com.android.quickstep.views.OverviewActionsView;
 import com.android.quickstep.views.RecentsView;
@@ -64,8 +65,8 @@ public class FallbackRecentsView extends RecentsView<RecentsActivity, RecentsSta
 
     @Override
     public void init(OverviewActionsView actionsView, SplitPlaceholderView splitPlaceholderView,
-                        MidClearAllButton midClearAllButton) {
-        super.init(actionsView, splitPlaceholderView, midClearAllButton);
+                        MidClearAllButton midClearAllButton, MemInfoView memInfoView) {
+        super.init(actionsView, splitPlaceholderView, midClearAllButton, memInfoView);
         setOverviewStateEnabled(true);
         setOverlayEnabled(true);
     }

quickstep/src/com/android/quickstep/views/MemInfoView.java
@@ -1,0 +1,154 @@
+/*
+ * Copyright (C) 2022 Project Kaleidoscope
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.quickstep.views;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.AttributeSet;
+import android.util.FloatProperty;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.FrameLayout.LayoutParams;
+import android.widget.TextView;
+
+import com.android.launcher3.anim.AlphaUpdateListener;
+import com.android.launcher3.DeviceProfile;
+import com.android.launcher3.util.MultiValueAlpha;
+import com.android.launcher3.R;
+import com.android.quickstep.SysUINavigationMode.Mode;
+
+import java.lang.Runnable;
+import java.math.BigDecimal;
+
+public class MemInfoView extends TextView {
+
+    // When to show GB instead of MB
+    private static final int UNIT_CONVERT_THRESHOLD = 1024; /* MiB */
+
+    private static final BigDecimal GB2MB = new BigDecimal(1024);
+
+    private static final int ALPHA_STATE_CTRL = 0;
+    public static final int ALPHA_FS_PROGRESS = 1;
+
+    public static final FloatProperty<MemInfoView> STATE_CTRL_ALPHA =
+            new FloatProperty<MemInfoView>("state control alpha") {
+                @Override
+                public Float get(MemInfoView view) {
+                    return view.getAlpha(ALPHA_STATE_CTRL);
+                }
+
+                @Override
+                public void setValue(MemInfoView view, float v) {
+                    view.setAlpha(ALPHA_STATE_CTRL, v);
+                }
+            };
+
+    private DeviceProfile mDp;
+    private MultiValueAlpha mAlpha;
+    private ActivityManager mActivityManager;
+
+    private Handler mHandler;
+    private MemInfoWorker mWorker;
+
+    private String mTranslatedTotal;
+    private String mTranslatedAvailable;
+
+    public MemInfoView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mAlpha = new MultiValueAlpha(this, 2);
+        mAlpha.setUpdateVisibility(true);
+        mActivityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        mHandler = new Handler(Looper.getMainLooper());
+        mWorker = new MemInfoWorker();
+
+        mTranslatedTotal = context.getResources().getString(R.string.meminfo_total);
+        mTranslatedAvailable = context.getResources().getString(R.string.meminfo_available);
+    }
+
+    /* Hijack this method to detect visibility rather than
+     * onVisibilityChanged() because the the latter one can be
+     * influenced by more factors, leading to unstable behavior. */
+    @Override
+    public void setVisibility(int visibility) {
+        super.setVisibility(visibility);
+
+        if (visibility == VISIBLE)
+            mHandler.post(mWorker);
+        else
+            mHandler.removeCallbacks(mWorker);
+    }
+
+    public void setDp(DeviceProfile dp) {
+        mDp = dp;
+    }
+
+    public void setAlpha(int alphaType, float alpha) {
+        mAlpha.getProperty(alphaType).setValue(alpha);
+    }
+
+    public float getAlpha(int alphaType) {
+        return mAlpha.getProperty(alphaType).getValue();
+    }
+
+    public void updateVerticalMargin(Mode mode) {
+        LayoutParams lp = (LayoutParams)getLayoutParams();
+        int bottomMargin;
+
+        if (mode == Mode.THREE_BUTTONS)
+            bottomMargin = mDp.memInfoMarginThreeButtonPx;
+        else
+            bottomMargin = mDp.memInfoMarginGesturePx;
+
+        lp.setMargins(lp.leftMargin, lp.topMargin, lp.rightMargin, bottomMargin);
+        lp.gravity = Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM;
+    }
+
+    private String unitConvert(long valueMiB, boolean alignToGB) {
+        BigDecimal rawVal = new BigDecimal(valueMiB);
+
+        if (alignToGB)
+            return rawVal.divide(GB2MB, 0, BigDecimal.ROUND_UP) + " GB";
+
+        if (valueMiB > UNIT_CONVERT_THRESHOLD)
+            return rawVal.divide(GB2MB, 1, BigDecimal.ROUND_HALF_UP) + " GB";
+        else
+            return rawVal + " MB";
+    }
+
+    private void updateMemInfoText(long availMemMiB, long totalMemMiB) {
+        String text = String.format("%s %s | %s %s",
+                        unitConvert(availMemMiB, false), mTranslatedAvailable,
+                        unitConvert(totalMemMiB, true), mTranslatedTotal);
+        setText(text);
+    }
+
+    private class MemInfoWorker implements Runnable {
+        @Override
+        public void run() {
+            ActivityManager.MemoryInfo memInfo = new ActivityManager.MemoryInfo();
+            mActivityManager.getMemoryInfo(memInfo);
+            long availMemMiB = memInfo.availMem / (1024 * 1024);
+            long totalMemMiB = memInfo.totalMem / (1024 * 1024);
+            updateMemInfoText(availMemMiB, totalMemMiB);
+
+            mHandler.postDelayed(this, 1000);
+        }
+    }
+}

DF-Whyred/packages_apps_DerpLauncher

Signed-off-by: pri0818 <priyanshusinghal0818@gmail.com>

quickstep/src/com/android/launcher3/BaseQuickstepLauncher.java
@@ -78,6 +78,7 @@ import com.android.quickstep.util.RemoteAnimationProvider;
 import com.android.quickstep.util.RemoteFadeOutAnimationListener;
 import com.android.quickstep.util.SplitSelectStateController;
 import com.android.quickstep.util.TISBindHelper;
+import com.android.quickstep.views.MemInfoView;
 import com.android.quickstep.views.MidClearAllButton;
 import com.android.quickstep.views.OverviewActionsView;
 import com.android.quickstep.views.RecentsView;
@@ -111,6 +112,7 @@ public abstract class BaseQuickstepLauncher extends Launcher
 
     private OverviewActionsView mActionsView;
     private MidClearAllButton mMidClearAllButton;
+    private MemInfoView mMemInfoView;
 
     private TISBindHelper mTISBindHelper;
     private @Nullable TaskbarManager mTaskbarManager;
@@ -191,6 +193,9 @@ public abstract class BaseQuickstepLauncher extends Launcher
         }
         if (mMidClearAllButton != null) {
             mMidClearAllButton.updateVerticalMargin(newMode);
+        }
+        if (mMemInfoView != null) {
+            mMemInfoView.updateVerticalMargin(newMode);
         }
     }
 
@@ -306,15 +311,18 @@ public abstract class BaseQuickstepLauncher extends Launcher
         SysUINavigationMode.INSTANCE.get(this).updateMode();
         mActionsView = findViewById(R.id.overview_actions_view);
         mMidClearAllButton = findViewById(R.id.mid_clear_all);
+        mMemInfoView = findViewById(R.id.meminfo);
         RecentsView overviewPanel = (RecentsView) getOverviewPanel();
         SplitSelectStateController controller =
                 new SplitSelectStateController(mHandler, SystemUiProxy.INSTANCE.get(this),
                         getStateManager(), getDepthController());
-        overviewPanel.init(mActionsView, controller, mMidClearAllButton);
+        overviewPanel.init(mActionsView, controller, mMidClearAllButton, mMemInfoView);
         mActionsView.setDp(getDeviceProfile());
         mMidClearAllButton.setDp(getDeviceProfile());
+        mMemInfoView.setDp(getDeviceProfile());
         mActionsView.updateVerticalMargin(SysUINavigationMode.getMode(this));
         mMidClearAllButton.updateVerticalMargin(SysUINavigationMode.getMode(this));
+        mMemInfoView.updateVerticalMargin(SysUINavigationMode.getMode(this));
 
         mAppTransitionManager = new QuickstepTransitionManager(this);
         mAppTransitionManager.registerRemoteAnimations();
@@ -370,6 +378,10 @@ public abstract class BaseQuickstepLauncher extends Launcher
 
     public MidClearAllButton getMidClearAllButton () {
         return mMidClearAllButton;
+    }
+
+    public MemInfoView getMemInfoView () {
+        return mMemInfoView;
     }
 
     @Override

quickstep/src/com/android/quickstep/RecentsActivity.java
@@ -80,6 +80,7 @@ import com.android.quickstep.SysUINavigationMode;
 import com.android.quickstep.util.RecentsAtomicAnimationFactory;
 import com.android.quickstep.util.SplitSelectStateController;
 import com.android.quickstep.util.TISBindHelper;
+import com.android.quickstep.views.MemInfoView;
 import com.android.quickstep.views.MidClearAllButton;
 import com.android.quickstep.views.OverviewActionsView;
 import com.android.quickstep.views.RecentsView;
@@ -114,6 +115,7 @@ public final class RecentsActivity extends StatefulActivity<RecentsState> {
     private @Nullable TaskbarManager mTaskbarManager;
     private @Nullable FallbackTaskbarUIController mTaskbarUIController;
     private MidClearAllButton mMidClearAllButton;
+    private MemInfoView mMemInfoView;
 
     private Configuration mOldConfig;
 
@@ -138,16 +140,20 @@ public final class RecentsActivity extends StatefulActivity<RecentsState> {
         mFallbackRecentsView = findViewById(R.id.overview_panel);
         mActionsView = findViewById(R.id.overview_actions_view);
         mMidClearAllButton = findViewById(R.id.mid_clear_all);
+        mMemInfoView= findViewById(R.id.meminfo);
         SYSUI_PROGRESS.set(getRootView().getSysUiScrim(), 0f);
 
         SplitSelectStateController controller =
                 new SplitSelectStateController(mHandler, SystemUiProxy.INSTANCE.get(this),
                         getStateManager(), null /*depthController*/);
         mDragLayer.recreateControllers();
-        mFallbackRecentsView.init(mActionsView, controller, mMidClearAllButton);
+        mFallbackRecentsView.init(mActionsView, controller, mMidClearAllButton, mMemInfoView);
 
         mMidClearAllButton.setDp(mDeviceProfile);
         mMidClearAllButton.updateVerticalMargin(SysUINavigationMode.getMode(this));
+
+        mMemInfoView.setDp(mDeviceProfile);
+        mMemInfoView.updateVerticalMargin(SysUINavigationMode.getMode(this));
 
         mTISBindHelper = new TISBindHelper(this, this::onTISConnected);
     }
@@ -231,6 +237,10 @@ public final class RecentsActivity extends StatefulActivity<RecentsState> {
 
     public MidClearAllButton getMidClearAllButton() {
         return mMidClearAllButton;
+    }
+
+    public MemInfoView getMemInfoView() {
+        return mMemInfoView;
     }
 
     @Override

quickstep/src/com/android/quickstep/fallback/FallbackRecentsView.java
@@ -44,6 +44,7 @@ import com.android.quickstep.RecentsActivity;
 import com.android.quickstep.util.GroupTask;
 import com.android.quickstep.util.SplitSelectStateController;
 import com.android.quickstep.util.TaskViewSimulator;
+import com.android.quickstep.views.MemInfoView;
 import com.android.quickstep.views.MidClearAllButton;
 import com.android.quickstep.views.OverviewActionsView;
 import com.android.quickstep.views.RecentsView;
@@ -70,8 +71,8 @@ public class FallbackRecentsView extends RecentsView<RecentsActivity, RecentsSta
 
     @Override
     public void init(OverviewActionsView actionsView, SplitSelectStateController splitController,
-                        MidClearAllButton midClearAllButton) {
-        super.init(actionsView, splitController, midClearAllButton);
+                        MidClearAllButton midClearAllButton, MemInfoView memInfoView) {
+        super.init(actionsView, splitController, midClearAllButton, memInfoView);
         setOverviewStateEnabled(true);
         setOverlayEnabled(true);
     }

quickstep/src/com/android/quickstep/views/MemInfoView.java
@@ -1,0 +1,151 @@
+/*
+ * Copyright (C) 2022 Project Kaleidoscope
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.quickstep.views;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.AttributeSet;
+import android.util.FloatProperty;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.FrameLayout.LayoutParams;
+import android.widget.TextView;
+
+import com.android.launcher3.anim.AlphaUpdateListener;
+import com.android.launcher3.DeviceProfile;
+import com.android.launcher3.util.MultiValueAlpha;
+import com.android.launcher3.R;
+import com.android.quickstep.SysUINavigationMode.Mode;
+
+import java.lang.Runnable;
+import java.math.BigDecimal;
+
+public class MemInfoView extends TextView {
+
+    // When to show GB instead of MB
+    private static final int UNIT_CONVERT_THRESHOLD = 1024; /* MiB */
+
+    private static final BigDecimal GB2MB = new BigDecimal(1024);
+
+    private static final int ALPHA_STATE_CTRL = 0;
+    public static final int ALPHA_FS_PROGRESS = 1;
+
+    public static final FloatProperty<MemInfoView> STATE_CTRL_ALPHA =
+            new FloatProperty<MemInfoView>("state control alpha") {
+                @Override
+                public Float get(MemInfoView view) {
+                    return view.getAlpha(ALPHA_STATE_CTRL);
+                }
+
+                @Override
+                public void setValue(MemInfoView view, float v) {
+                    view.setAlpha(ALPHA_STATE_CTRL, v);
+                }
+            };
+
+    private DeviceProfile mDp;
+    private MultiValueAlpha mAlpha;
+    private ActivityManager mActivityManager;
+
+    private Handler mHandler;
+    private MemInfoWorker mWorker;
+
+    private String mMemInfoText;
+
+    public MemInfoView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mAlpha = new MultiValueAlpha(this, 2);
+        mAlpha.setUpdateVisibility(true);
+        mActivityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        mHandler = new Handler(Looper.getMainLooper());
+        mWorker = new MemInfoWorker();
+
+        mMemInfoText = context.getResources().getString(R.string.meminfo_text);
+    }
+
+    /* Hijack this method to detect visibility rather than
+     * onVisibilityChanged() because the the latter one can be
+     * influenced by more factors, leading to unstable behavior. */
+    @Override
+    public void setVisibility(int visibility) {
+        super.setVisibility(visibility);
+
+        if (visibility == VISIBLE)
+            mHandler.post(mWorker);
+        else
+            mHandler.removeCallbacks(mWorker);
+    }
+
+    public void setDp(DeviceProfile dp) {
+        mDp = dp;
+    }
+
+    public void setAlpha(int alphaType, float alpha) {
+        mAlpha.getProperty(alphaType).setValue(alpha);
+    }
+
+    public float getAlpha(int alphaType) {
+        return mAlpha.getProperty(alphaType).getValue();
+    }
+
+    public void updateVerticalMargin(Mode mode) {
+        LayoutParams lp = (LayoutParams)getLayoutParams();
+        int bottomMargin;
+
+        if (mode == Mode.THREE_BUTTONS)
+            bottomMargin = mDp.memInfoMarginThreeButtonPx;
+        else
+            bottomMargin = mDp.memInfoMarginGesturePx;
+
+        lp.setMargins(lp.leftMargin, lp.topMargin, lp.rightMargin, bottomMargin);
+        lp.gravity = Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM;
+    }
+
+    private String unitConvert(long valueMiB, boolean alignToGB) {
+        BigDecimal rawVal = new BigDecimal(valueMiB);
+
+        if (alignToGB)
+            return rawVal.divide(GB2MB, 0, BigDecimal.ROUND_UP) + " GB";
+
+        if (valueMiB > UNIT_CONVERT_THRESHOLD)
+            return rawVal.divide(GB2MB, 1, BigDecimal.ROUND_HALF_UP) + " GB";
+        else
+            return rawVal + " MB";
+    }
+
+    private void updateMemInfoText(long availMemMiB, long totalMemMiB) {
+        String text = String.format(mMemInfoText,
+            unitConvert(availMemMiB, false), unitConvert(totalMemMiB, true));
+        setText(text);
+    }
+
+    private class MemInfoWorker implements Runnable {
+        @Override
+        public void run() {
+            ActivityManager.MemoryInfo memInfo = new ActivityManager.MemoryInfo();
+            mActivityManager.getMemoryInfo(memInfo);
+            long availMemMiB = memInfo.availMem / (1024 * 1024);
+            long totalMemMiB = memInfo.totalMem / (1024 * 1024);
+            updateMemInfoText(availMemMiB, totalMemMiB);
+
+            mHandler.postDelayed(this, 1000);
+        }
+    }
+}